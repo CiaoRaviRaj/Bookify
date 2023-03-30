1f19653389e3c27f44f4129ebfee7afc9769ddc9
ciaoraviraj <ravir@zignuts.com>
2023-03-30 16:52:16 +0530

    hhhhh

--- src/pages/orders.js
+++ src/pages/orders.js
@@ -19,6 +19,7 @@ function orders({ data1 }) {
   const { data: session } = useSession()
   const orders = data1 //JSON.parse(data1)
   console.log(orders)
+  console.log(session)
   return (
     <div>
       <Header />

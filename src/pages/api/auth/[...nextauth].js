2e33109dfd2e860f8fe217c49f913c0b4b22d454
ciaoraviraj <ravir@zignuts.com>
2023-03-30 13:29:54 +0530

    test 1

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,17 +1,15 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
-
 export default NextAuth({
-
   providers: [
     GoogleProvider({
       clientId: process.env.google_id,
       clientSecret: process.env.google_secret,
     }),
   ],
-  // pages: {
-  //   signIn: '/signin',
-  // },
+  pages: {
+    signIn: '/signin',
+  },
   secret: process.env.NEXTAUTH_SECRET,
 })
